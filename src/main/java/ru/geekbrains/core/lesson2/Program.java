404acde8f096ca25dbdd7bdcf4414c78647da6c0
VxElA-A <aaleksandrenko@gmail.com>
2024-05-05 22:55:47 +0300

    Initial commit - Lesson 2

---
+++ src/main/java/ru/geekbrains/core/lesson2/Program.java
@@ -0,0 +1,182 @@
+package ru.geekbrains.core.lesson2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Program {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    public static final Random random = new Random();
+    public static final int WIN_COUNT = 3;
+    public static final char DOT_HUMAN = 'X';
+    public static final char DOT_AI = '0';
+    public static final char DOT_EMPTY = '*';
+    private static int fieldSizeX;
+    private static int fieldSizeY;
+    private static char[][] field;
+
+
+
+    public static void main(String[] args) {
+        while (true){
+            initialize();
+            printField();
+            while (true){
+                humanTurn();
+                printField();
+                if (checkState(DOT_HUMAN, "Вы победили!"))
+                    break;
+                aiTurn();
+                printField();
+                if (checkState(DOT_AI, "Компьютер победил!"))
+                    break;
+            }
+            System.out.println("Желаете сыграть еще раз? (Y - да): ");
+            if (!scanner.next().equalsIgnoreCase("Y"))
+                break;
+        }
+    }
+
+    /**
+     * Инициализация объектов игры - создание метода первоначального (на момент начала игры) состояния игрового поля
+     */
+    static void initialize(){
+        fieldSizeX = 3;
+        fieldSizeY = 3;
+        field = new char[fieldSizeX][fieldSizeY];
+        for (int x = 0; x < fieldSizeX; x++) {
+            for (int y = 0; y < fieldSizeY; y++) {
+                field[x][y] = DOT_EMPTY;
+            }
+        }
+    }
+
+    /**
+     * Печать (Вывод на экран) текущего состояния игрового поля
+     */
+    static void printField(){
+        System.out.print("+");
+        for (int x = 0; x < fieldSizeX; x++){
+            System.out.print("-" + (x + 1));
+        }
+        System.out.println("-");
+
+        for (int x = 0; x < fieldSizeX; x++) {
+            System.out.print(x + 1 + "|");
+            for (int y = 0; y < fieldSizeY; y++) {
+                System.out.print(field[x][y] + "|");
+            }
+            System.out.println();
+        }
+
+        for (int x = 0; x < fieldSizeX * 2 + 2; x++){
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Ход человека - приглашение сделать первый ход
+     */
+    static void humanTurn(){
+        int x;
+        int y;
+        do {
+            System.out.println("Введите координаты хода X и Y\n(от 1 до 3) через пробел: ");
+            x = scanner.nextInt() - 1;
+            y = scanner.nextInt() - 1;
+        }
+        while (!isCellValid(x, y) || !isCellEmpty(x, y));
+        field[x][y] = DOT_HUMAN;
+    }
+
+    /**
+     * Прверка ячейки - пустая или нет
+     * @param x
+     * @param y
+     * @return
+     */
+    static boolean isCellEmpty(int x, int y){
+        return field[x][y] == DOT_EMPTY;
+    }
+
+    /**
+     * Проверка валидности координат хода
+     * @param x
+     * @param y
+     * @return
+     */
+    static boolean isCellValid(int x, int y){
+        return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
+    }
+
+    /**
+     * Ход компьютера
+     */
+    static void aiTurn(){
+        int x;
+        int y;
+        do{
+            x = random.nextInt(fieldSizeX);
+            y = random.nextInt(fieldSizeY);
+        }
+        while (!isCellEmpty(x, y));
+        field[x][y] = DOT_AI;
+    }
+
+    /**
+     * Проверка на ничью
+     * @return
+     */
+    static boolean checkDraw(){
+        for (int x = 0; x < fieldSizeX; x++) {
+            for (int y = 0; y < fieldSizeY; y++) {
+                if (isCellEmpty(x, y)) return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * TODO переработать в рамках домашней работы (Не могу)
+     * Метод проверки победы
+     * @param dot фишка игрока
+     * @return
+     */
+    static boolean checkWin(char dot){
+        // Проверка победы по горизонтали
+        if (field[0][0] == dot && field[0][1] == dot && field[0][2] == dot) return true;
+        if (field[1][0] == dot && field[1][1] == dot && field[1][2] == dot) return true;
+        if (field[2][0] == dot && field[2][1] == dot && field[2][2] == dot) return true;
+
+        // Проверка победы по вертикали
+        if (field[0][0] == dot && field[1][0] == dot && field[2][0] == dot) return true;
+        if (field[0][1] == dot && field[1][1] == dot && field[2][1] == dot) return true;
+        if (field[0][2] == dot && field[1][2] == dot && field[2][2] == dot) return true;
+
+        // Проверка победы по двум диагоналям
+        if (field[0][0] == dot && field[1][1] == dot && field[2][2] == dot) return true;
+        if (field[0][2] == dot && field[1][1] == dot && field[2][0] == dot) return true;
+
+        return false;
+    }
+
+    /**
+     * Проверка состояния игры
+     * @param dot фишка игрока
+     * @param s победный слоган
+     * @return
+     */
+    static boolean checkState(char dot, String s){
+        if (checkWin(dot)){
+            System.out.println(s);
+            return true;
+        }
+        else if (checkDraw()) {
+        System.out.println("Ничья!");
+        return true;
+        }
+        return false; // Игра продолжается
+    }
+
+}
